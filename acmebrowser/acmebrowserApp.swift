c50162fc3af41abf93b09e43e9f1bef95e8ba67f
Victor Idongesit <victoridongesit@gmail.com>
2022-04-11 17:02:49 +0100

    Initial Commit

---
+++ acmebrowser/acmebrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  acmebrowserApp.swift
+//  acmebrowser
+//
+//  Created by Victor Idongesit on 11/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct acmebrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
